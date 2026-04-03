4971e5e40c5d781be80e84ca22674d43571776c5
Bhavya <choprabhavya41@gmail.com>
2026-04-03 09:44:47 +0530

    Add employee count procedure by gender
    Added a stored procedure to count employees by gender and output the status.

--- Experiment_8/Experiment_8.sql
+++ Experiment_8/Experiment_8.sql
@@ -9,37 +9,32 @@ INSERT INTO employees (emp_name, gender, salary) VALUES
 ('Amit', 'Male', 30000),
 ('Riya', 'Female', 35000),
 ('John', 'Male', 28000),
-('Sneha', 'Female', 40000)
+('Sneha', 'Female', 40000);
 
+CREATE OR REPLACE PROCEDURE get_employee_count_by_gender(
+    IN p_gen VARCHAR(20),
+    OUT count_emp INT,
+    INOUT status VARCHAR
+)
+AS $$
+BEGIN
+    SELECT COUNT(*) INTO count_emp
+    FROM employees
+    WHERE gender = p_gen;
 
---count numbe of employees based on Gender
+    status := 'SUCCESS';
+END;
+$$ LANGUAGE plpgsql;
 
+DO $$
+DECLARE
+    gen VARCHAR(20) := 'Male';
+    count_by_gen INT;
+    status VARCHAR(20) := 'Fail';
+BEGIN
+    CALL get_employee_count_by_gender(gen, count_by_gen, status);
 
-SELECT * FROM EMPLOYEES;
-
-CREATE OR REPLACE PROCEDURE get_Employee_Count_BY_Gender(IN P_GEN VARCHAR(20), OUT COUNT_EMP INT, INOUT STATUS VARCHAR)
-AS
-	$$
-		BEGIN
-
-			SELECT COUNT(*) INTO COUNT_EMP FROM EMPLOYEES WHERE GENDER=P_GEN;
-			STATUS:='SUCCESS';
-        END;
-	 $$ LANGUAGE PLPGSQL
-
-
-		DO
-		$$
-		DECLARE
-			GEN VARCHAR(20):='Male';
-			COUNT_BY_GEN INT;
-			STATUS VARCHAR(20):='Fail';
-		BEGIN
-
-			CALL  get_Employee_Count_BY_Gender(GEN,COUNT_BY_GEN,STATUS);
-
-			RAISE NOTICE 'GENDER IS % YOUR COUNT IS %  AND STATUS IS %',GEN,COUNT_BY_GEN,STATUS;
-			
-		END;
-
-        $$
+    RAISE NOTICE 'GENDER IS % YOUR COUNT IS % AND STATUS IS %',
+        gen, count_by_gen, status;
+END;
+$$;
